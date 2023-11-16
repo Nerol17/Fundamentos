a22254e935358ac27549962078c723bd53cfbd27
Nerol17 <mario2181443@correo.uis.edu.co>
2023-11-15 21:56:43 -0500

    Ejercicio 5 del parcial en Python

--- 5.py
+++ 5.py
@@ -1,4 +1,5 @@
 import random as rnd
+import numpy as np
 
 n = int(input("Ingrese el numero de elementos: "))
 
@@ -6,15 +7,20 @@ elementos = [ ]
 
 for i in range(n) :
   elementos.append(rnd.randint(1, 10))
-  print(elementos[i])
+  print(elementos[i], end="  ")
 
-suma = 0
-for i in range(n) :
-  suma += elementos
-
-media = suma / n
+print("")
+print("")
+media = np.sum(elementos) / n
 print("La media del arreglo es: ", media)
+print("")
 
+print("Elementos mayores a la media: ")
+mayor = [ ]
+j = 0
 for i in range(n) :
-  if (elementos > media) :
-    print(elementos [i])
+  if (elementos[i] > media) :
+    mayor.append(elementos[i])
+
+mayor.sort()
+print(mayor)
